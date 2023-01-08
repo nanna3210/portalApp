e75b6436ecd3fd059d1a93bf5820ff24ffd34d6d
nanna <navkishoredas@gmail.com>
2023-01-09 05:02:39 +0530

    added security and implemented UserDetails from spring security

---
+++ src/main/java/com/nanna/portal/resource/UserResource.java
@@ -0,0 +1,19 @@
+package com.nanna.portal.resource;
+
+import com.nanna.portal.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping("/home")
+    public String  showUser() {
+        return "application working ";
+    }
+
+
+
+}
