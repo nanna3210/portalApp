6dbf653640d714b1dbc83104cf4f6332e8de3ea9
nanna <navkishoredas@gmail.com>
2023-01-09 03:19:32 +0530

    added security and implemented UserDetails from spring security

---
+++ src/main/java/com/nanna/portal/domain/HttpResponse.java
@@ -0,0 +1,57 @@
+package com.nanna.portal.domain;
+
+import org.springframework.http.HttpStatus;
+
+//import org.springframework
+public class HttpResponse {
+
+    
+    private int HtpStatusCode;
+    //    private
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+    
+    public HttpResponse ( ) {
+    }
+    
+    
+    public HttpResponse ( int htpStatusCode , HttpStatus httpStatus , String reason , String message ) {
+        HtpStatusCode   = htpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason     = reason;
+        this.message    = message;
+    }
+    
+    public int getHtpStatusCode ( ) {
+        return HtpStatusCode;
+    }
+    
+    public void setHtpStatusCode ( int htpStatusCode ) {
+        HtpStatusCode = htpStatusCode;
+    }
+    
+    public HttpStatus getHttpStatus ( ) {
+        return httpStatus;
+    }
+    
+    public void setHttpStatus ( HttpStatus httpStatus ) {
+        this.httpStatus = httpStatus;
+    }
+    
+    public String getReason ( ) {
+        return reason;
+    }
+    
+    public void setReason ( String reason ) {
+        this.reason = reason;
+    }
+    
+    public String getMessage ( ) {
+        return message;
+    }
+    
+    public void setMessage ( String message ) {
+        this.message = message;
+    }
+}
