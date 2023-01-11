e5a3224074dc89f7c2d4fdec33075ad62e692118
nanna <navkishoredas@gmail.com>
2023-01-12 01:59:15 +0530

    jwt dependency added in POM

---
+++ src/main/java/com/nanna/portal/constant/SecurityConstant.java
@@ -0,0 +1,18 @@
+package com.nanna.portal.constant;
+
+public class SecurityConstant {
+    
+    public static final long EXPIRATION_TIME = 432_000_000;
+    public static final String TOKEN_PREFIX = "Bear ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token Cannot Be Verified";
+    public static final String GET_ARRAYS_LLC = "Get Arrays , LLC";
+    public static final String GET_ARRAYS_ADMINISTRATION = "User Management  Portal";
+    public static final String AUTHORITIES = "authorities";
+    public static final String FORBIDDEN_MESSAGE = "you need to login to access this page";
+    public static final String ACCESS_DENIED = "YOU DO NOT HAVE PERMISSION TO  ACCESS THIS PAGE ";
+    public static final String OPTIONS_HHTP_METHOD = "OPTIONS ";
+    public static final String[] PUBLIC_URLS = { "/user/login" , "/user/register" , "/user/resetpassword/**" ,"/user" +
+            "/image/**"};
+    
+}
---
+++ src/main/java/com/nanna/portal/utlity/JWTTokenProvider.java
@@ -0,0 +1,6 @@
+package com.nanna.portal.utlity;
+
+public class JWTTokenProvider {
+
+
+}
