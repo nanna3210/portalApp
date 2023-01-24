1f65aad7d871bd1c4c9584907bb690dacc24024e
nanna <navkishoredas@gmail.com>
2023-01-25 02:41:11 +0530

    dofilterInternal

--- src/main/java/com/nanna/portal/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/nanna/portal/filter/JwtAuthorizationFilter.java
@@ -1,5 +1,63 @@
 package com.nanna.portal.filter;
 
-public class JwtAuthorizationFilter {
+import com.nanna.portal.constant.SecurityConstant;
+import com.nanna.portal.utlity.JWTTokenProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+import java.util.List;
+
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    
+//    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+    
+    public JwtAuthorizationFilter ( JWTTokenProvider jwtTokenProvider ) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+    
+    @Override
+    protected void doFilterInternal ( HttpServletRequest request , HttpServletResponse response , FilterChain filterChain ) throws ServletException, IOException {
+    
+        if(request.getMethod ().equalsIgnoreCase ( SecurityConstant.OPTIONS_HHTP_METHOD ))
+        {
+            response.setStatus ( HttpStatus.OK.value () );
+        }
+        else  {
+            String authorizationHeader = request.getHeader ( HttpHeaders.AUTHORIZATION );
+            if(authorizationHeader != null || authorizationHeader.startsWith ( SecurityConstant.TOKEN_PREFIX )) {
+    
+                filterChain.doFilter ( request, response );
+                return ;
+            }
+            String token = authorizationHeader.substring ( SecurityConstant.TOKEN_PREFIX.length () );
+            String username = jwtTokenProvider.getSubject ( token );
+            if(jwtTokenProvider.isTokenvalid ( username, token ) && SecurityContextHolder.getContext ().getAuthentication ()==null ) {
+                List < GrantedAuthority > authorities = jwtTokenProvider.getAuthorities ( token );
+                
+            }
+        }
+    
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
